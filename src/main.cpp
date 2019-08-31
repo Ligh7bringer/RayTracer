600ba6cc3656e2bb5f9c1b92a5e4279b23ce144b
Svetlozar Georgiev <sfe7lio@gmail.com>
2019-08-31 21:57:02 +0100

    Multiple spheres can now be added to the scene

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,41 @@
 #include <fstream>
 #include <iostream>
+#include <limits>
 
-#include "ray.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-bool hit_sphere(const vec3& centre, float radius, const ray& r)
+//float hit_sphere(const vec3& centre, float radius, const ray& r)
+//{
+//	vec3 oc = r.origin() - centre;
+//	float a = dot(r.direction(), r.direction());
+//	float b = 2.f * dot(oc, r.direction());
+//	float c = dot(oc, oc) - radius * radius;
+//	float discriminant = b * b - 4 * a * c;
+//
+//	if(discriminant < 0.f)
+//	{
+//		return -1.f;
+//	}
+//	else
+//	{
+//		return (-b - sqrt(discriminant)) / (2.f * a);
+//	}
+//}
+
+vec3 colour(const ray& r, hittable* world)
 {
-	vec3 oc = r.origin() - centre;
-	float a = dot(r.direction(), r.direction());
-	float b = 2.f * dot(oc, r.direction());
-	float c = dot(oc, oc) - radius * radius;
-	float discriminant = b * b - 4 * a * c;
-	return (discriminant > 0);
-}
-
-vec3 colour(const ray& r)
-{
-	if(hit_sphere(vec3(0, 0, -1), 0.5, r))
-		return vec3(1, 0, 0);
-
-	vec3 unit_direction = unit_vector(r.direction());
-	float t = 0.5f * static_cast<float>((unit_direction.y() + 1.0f));
-	return (1.f - t) * vec3(1.f, 1.f, 1.f) + t * vec3(0.5f, 0.7f, 1.0f);
+	hit_record rec;
+	if(world->hit(r, 0.f, std::numeric_limits<float>::max(), rec))
+	{
+		return 0.5f * vec3(rec.normal.x() + 1.f, rec.normal.y() + 1.f, rec.normal.z() + 1.f);
+	}
+	else
+	{
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5f * (unit_direction.y() + 1.0f);
+		return (1.f - t) * vec3(1.f, 1.f, 1.f) + t * vec3(0.5f, 0.7f, 1.0f);
+	}
 }
 
 int main()
@@ -38,6 +53,11 @@ int main()
 	const vec3 vertical(0.f, 2.f, 0.f);
 	const vec3 origin(0.f, 0.f, 0.f);
 
+	hittable* list[2];
+	list[0] = new sphere(vec3(0.f, 0.f, -1.f), 0.5f);
+	list[1] = new sphere(vec3(0.f, -100.5f, -1.f), 100.f);
+	hittable* world = new hittable_list(list, 2);
+
 	for(int j = ny - 1; j >= 0; j--)
 	{
 		for(int i = 0; i < nx; i++)
@@ -47,7 +67,7 @@ int main()
 
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
 
-			vec3 col = colour(r);
+			vec3 col = colour(r, world);
 			int ir = int(255.99 * col.r());
 			int ig = int(255.99 * col.g());
 			int ib = int(255.99 * col.b());
---
+++ src/sphere.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "hittable.h"
+
+class sphere : public hittable
+{
+public:
+	sphere() = default;
+	sphere(vec3 cen, float r)
+		: centre(cen)
+		, radius(r)
+	{}
+
+	virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const override
+	{
+		vec3 oc = r.origin() - centre;
+		float a = dot(r.direction(), r.direction());
+		float b = dot(oc, r.direction());
+		float c = dot(oc, oc) - radius * radius;
+		float discriminant = b * b - a * c;
+
+		if(discriminant > 0.f)
+		{
+			float temp = (-b - sqrt(b * b - a * c)) / a;
+			if(temp < t_max && temp > t_min)
+			{
+				rec.t = temp;
+				rec.p = r.point_at_parameter(rec.t);
+				rec.normal = (rec.p - centre) / radius;
+				return true;
+			}
+
+			temp = (-b + sqrt(b * b - a * c)) / a;
+			if(temp < t_max && temp > t_min)
+			{
+				rec.t = temp;
+				rec.p = r.point_at_parameter(rec.t);
+				rec.normal = (rec.p - centre) / radius;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+private:
+	vec3 centre;
+	float radius;
+};
