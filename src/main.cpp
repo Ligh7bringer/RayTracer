5c1bc92593c169d58eeed826eb83da624afe6104
Svetlozar Georgiev <sfe7lio@gmail.com>
2019-08-31 21:11:39 +0100

    Added a sphere to the scene
    It looks like a circle as shading is not supported yet; it is alsohardcoded.

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,29 @@
 
 #include "ray.h"
 
+bool hit_sphere(const vec3& centre, float radius, const ray& r)
+{
+	vec3 oc = r.origin() - centre;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.f * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4 * a * c;
+	return (discriminant > 0);
+}
+
 vec3 colour(const ray& r)
 {
-	auto unit_direction = unit_vector(r.direction());
+	if(hit_sphere(vec3(0, 0, -1), 0.5, r))
+		return vec3(1, 0, 0);
+
+	vec3 unit_direction = unit_vector(r.direction());
 	float t = 0.5f * static_cast<float>((unit_direction.y() + 1.0f));
 	return (1.f - t) * vec3(1.f, 1.f, 1.f) + t * vec3(0.5f, 0.7f, 1.0f);
 }
 
 int main()
 {
-	int nx = 800;
+	int nx = 1200;
 	int ny = 600;
 
 	std::ofstream out;
